ad904540775f37f9e18565010ebd17301f68f8cc
Mamun Mia <70141554+Mamun-Mia1078@users.noreply.github.com>
2022-06-08 00:37:04 +0600

    first solved lightOJ

---
+++ LightOJ/loj1000.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int T;
+    cin >> T;
+    for(int i = 1; i <= T;i++){
+        int a , b;
+        cin >> a >> b;
+        int sum = a + b;
+        printf("Case %d: %d\n",i,sum);
+    }
+    return 0;
+
+}
+
