e818a12fa04bdbe1f2a89f5568bca37305cba7ef
Mamun Mia <70141554+Mamun-Mia1078@users.noreply.github.com>
2022-06-12 13:24:10 +0600

    basic problem of freeCodeCamp

---
+++ FreecodeCamp with JS/freeCodeCampBasicProblem.js
@@ -0,0 +1,50 @@
+// Declare a Read-Only Variable with the const Keyword freecodecamp solution
+const FCC = "freeCodeCamp";
+let fact = "is cool!";
+fact = "is awesome";
+console.log(FCC,fact)
+
+// Add Two Numbers with JavaScript
+const sum = 10 + 10;
+console.log(sum);
+
+//  Subtract One Number from Another with JavaScript
+const difference = 45 - 33;
+console.log(difference)
+
+// Multiply Two Numbers with JavaScript
+const product = 8 * 10;
+console.log(product);
+
+//Divide One Number by Another with JavaScript
+const quotient = 66 / 33;
+console.log(quotient);
+
+//Increment a Number with JavaScript
+
+let myVar1 = 87;
+
+// Only change code below this line
+myVar ++;
+console.log(myVar1)
+
+
+// Decrement a Number with JavaScript
+let myVar = 11;
+
+// Only change code below this line
+myVar--;
+console.log(myVar);
+
+// Create Decimal Numbers with JavaScript
+const ourDecimal = 5.7;
+
+// Only change code below this line
+const myDecimal = 5.7;
+console.log(myDecimal)
+
+// Multiply Two Decimals with JavaScript
+
+const products = 5.0 * 1;
+console.log(product)
+
--- LightOJ/loj1000.cpp
+++ LightOJ/loj1000.cpp
@@ -7,7 +7,7 @@ int main()
     for(int i = 1; i <= T;i++){
         int a , b;
         cin >> a >> b;
-        int sum = a + b;
+        int sum = b-a;
         printf("Case %d: %d\n",i,sum);
     }
     return 0;
