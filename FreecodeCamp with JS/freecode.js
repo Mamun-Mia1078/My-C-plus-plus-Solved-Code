254e29547a7db745dff8896c0c7395da12a72a33
Mamun Mia <70141554+Mamun-Mia1078@users.noreply.github.com>
2022-06-19 14:02:43 +0600

    Solved code

--- FreecodeCamp with JS/freecode.js
+++ FreecodeCamp with JS/freecode.js
@@ -1,2 +1,34 @@
-// Concatenating Strings with Plus Operator
+//* Concatenating Strings with Plus Operator
 const myStr = "This is the start." + " This is the end.";// Change this line
+
+// *Store Multiple Values in one Variable using JavaScript Arrays
+const myArray = ['myArray',34];
+// *Nest one Array within Another Array
+const myArrays =[ ["Mamun",25], ["white box",55]];
+
+//* Find the Length of a String
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+
+// *Use Bracket Notation to Find the First Character in a String
+
+let firstLetterOfLastName = "";
+const lastNames = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+
+//*Word Blanks
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+var wordBlanks = "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + "."; // Only change this line;
+// Only change code above this line
