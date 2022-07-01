313c325bfc218dd2756860192fee4d5d17d4b400
Mamun Mia <70141554+Mamun-Mia1078@users.noreply.github.com>
2022-07-01 23:16:21 +0600

    solved code

--- FreecodeCamp with JS/81 to/solveddd.js
+++ FreecodeCamp with JS/81 to/solveddd.js
@@ -27,4 +27,20 @@ function chainToSwitch(val) {
   chainToSwitch(7);
 
 
-  //* 
+  //* Return Early Pattern for Functions
+  function abTest(a, b) {
+    // Only change code below this line
+    if (a < 0 || b < 0) {
+      return undefined;
+    }
+  
+    // Only change code above this line
+  
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+  }
+  
+  // Change values below to test your code
+  abTest(2, 2);
+
+
+  //*
