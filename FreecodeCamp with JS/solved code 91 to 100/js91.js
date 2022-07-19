4dbfa48d8b3194aaf42cf1aa65df2e1acae1d222
Mamun Mia <70141554+Mamun-Mia1078@users.noreply.github.com>
2022-07-19 21:56:05 +0600

    solved code

--- FreecodeCamp with JS/solved code 91 to 100/js91.js
+++ FreecodeCamp with JS/solved code 91 to 100/js91.js
@@ -97,3 +97,17 @@ var myPlants = [
 
 var secondTree = myPlants[1].list[1];
 
+
+
+//*96 Iterate with JavaScript While Loops
+
+
+// Setup
+const myArray = [];
+
+// Only change code below this line.
+let i = 5;
+while(i >= 0) {
+  myArray.push(i);
+  i--;
+}
