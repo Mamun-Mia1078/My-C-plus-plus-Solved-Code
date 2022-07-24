8544a8326cd03e261b3aafd416166c08088f42f1
Mamun Mia <70141554+Mamun-Mia1078@users.noreply.github.com>
2022-07-24 14:28:52 +0600

    solved code

--- FreecodeCamp with JS/101 to 110/fromhundred.js
+++ FreecodeCamp with JS/101 to 110/fromhundred.js
@@ -1,4 +1,4 @@
-//* Iterate Through an Array with a For Loop
+//*101 Iterate Through an Array with a For Loop
 // Setup
 const myArr = [2, 3, 4, 5, 6];
 
@@ -7,3 +7,26 @@ let total = 0;
 for (let i = 0; i < myArr.length; i++) {
   total += myArr[i];
 }
+
+
+//*102 Nesting For Loops
+
+
+
+function multiplyAll(arr) {
+  var product = 1;
+  // Only change code below this line
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr[i].length; j++) {
+      product = product * arr[i][j];
+    }
+  }
+  // Only change code above this line
+  return product;
+}
+
+// Modify values below to test your code
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+
+
+//*103 Iterate with JavaScript Do...While Loops
