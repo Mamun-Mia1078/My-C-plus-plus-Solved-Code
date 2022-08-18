6aa257518d3f14887e0a3c98ea7119dc834e8aec
Mamun Mia <70141554+Mamun-Mia1078@users.noreply.github.com>
2022-08-19 03:02:37 +0600

    solved code

--- FreecodeCamp with JS/101 to 110/fromhundred.js
+++ FreecodeCamp with JS/101 to 110/fromhundred.js
@@ -78,3 +78,20 @@ function rangeOfNumbers(startNum, endNum) {
     return numbers;
   }
 }
+
+
+
+//* 109 Iterate with JavaScript Do...While Loops
+
+// Setup
+const myArray = [];
+let i = 10;
+
+// Only change code below this line
+
+do {
+  myArray.push(i);
+  i++;
+} while (i <= 10);
+
+//* 110
