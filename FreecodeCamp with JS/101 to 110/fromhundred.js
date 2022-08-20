5d0c0bba75cb31c1ca8b377cf636c0fb09c02c3f
Mamun Mia <70141554+Mamun-Mia1078@users.noreply.github.com>
2022-08-20 11:43:33 +0600

    problem Solving Code

--- FreecodeCamp with JS/101 to 110/fromhundred.js
+++ FreecodeCamp with JS/101 to 110/fromhundred.js
@@ -94,4 +94,15 @@ do {
   i++;
 } while (i <= 10);
 
-//* 110
+//* 110 Generate Random Whole Numbers with JavaScript
+
+
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+  // Only change code below this line.
+  return Math.floor(Math.random() * 10);
+}
+
+
+///* 111 
